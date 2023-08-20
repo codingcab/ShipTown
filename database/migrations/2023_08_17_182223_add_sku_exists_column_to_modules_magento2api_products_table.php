0264efa9cf880abab9b07b17e9b6fd0f2f03dc2e
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-20 07:38:16 +0200

    add column exists check

--- database/migrations/2023_08_17_182223_add_sku_exists_column_to_modules_magento2api_products_table.php
+++ database/migrations/2023_08_17_182223_add_sku_exists_column_to_modules_magento2api_products_table.php
@@ -8,6 +8,10 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (Schema::hasColumn('modules_magento2api_products', 'exists_in_magento')) {
+            return;
+        }
+
         Schema::table('modules_magento2api_products', function (Blueprint $table) {
             $table->boolean('exists_in_magento')->nullable()->after('product_id');
         });
