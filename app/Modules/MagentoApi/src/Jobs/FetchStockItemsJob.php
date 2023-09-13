0708b2570e587e9473055a8786b78a6302cb297e
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-13 22:32:57 +0200

    refactoring

--- app/Modules/MagentoApi/src/Jobs/FetchStockItemsJob.php
+++ app/Modules/MagentoApi/src/Jobs/FetchStockItemsJob.php
@@ -2,31 +2,13 @@
 
 namespace App\Modules\MagentoApi\src\Jobs;
 
+use App\Abstracts\UniqueJob;
 use App\Modules\MagentoApi\src\Models\MagentoProduct;
-use App\Modules\MagentoApi\src\Models\MagentoProductInventoryComparisonView;
 use App\Modules\MagentoApi\src\Services\MagentoService;
 use Exception;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
-/**
- * Class SyncCheckFailedProductsJob.
- */
-class FetchStockItemsJob implements ShouldQueue
+class FetchStockItemsJob extends UniqueJob
 {
-    use Dispatchable;
-    use InteractsWithQueue;
-    use Queueable;
-    use SerializesModels;
-
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
         MagentoProduct::query()
