ea41cf1295ebeacf23ab6f0583a25c38b983d20c
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-15 16:09:03 +0200

    update job

--- app/Modules/InventoryTotals/src/Jobs/EnsureTotalsByWarehouseTagRecordsExistJob.php
+++ app/Modules/InventoryTotals/src/Jobs/EnsureTotalsByWarehouseTagRecordsExistJob.php
@@ -4,57 +4,69 @@ namespace App\Modules\InventoryTotals\src\Jobs;
 
 use App\Abstracts\UniqueJob;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class EnsureTotalsByWarehouseTagRecordsExistJob extends UniqueJob
 {
     public function handle()
     {
-        DB::statement("DROP TEMPORARY TABLE IF EXISTS tempTable;");
+        $maxRounds = 100;
 
-        DB::statement("
-            CREATE TEMPORARY TABLE tempTable AS
-            SELECT
-                taggables.tag_id, inventory.product_id
-            FROM taggables
+        do {
+            Log::debug('EnsureTotalsByWarehouseTagRecordsExistJob: rounds left ' . $maxRounds);
 
-            INNER JOIN inventory
-                ON inventory.warehouse_id = taggables.taggable_id
+            DB::statement("DROP TEMPORARY TABLE IF EXISTS tempTable;");
 
-            LEFT JOIN inventory_totals_by_warehouse_tag
-                ON inventory_totals_by_warehouse_tag.product_id = inventory.product_id
-                AND inventory_totals_by_warehouse_tag.tag_id = taggables.tag_id
+            DB::statement("
+                CREATE TEMPORARY TABLE tempTable AS
+                SELECT
+                    distinct taggables.tag_id, inventory.product_id, taggables.taggable_id as warehouse_id
+                FROM taggables
 
-            WHERE
-                taggables.taggable_type = 'App\\Models\\Warehouse'
-                AND inventory_totals_by_warehouse_tag.id IS NULL
+                INNER JOIN inventory
+                    ON inventory.warehouse_id = taggables.taggable_id
 
-            LIMIT 10000;
-        ");
+                LEFT JOIN inventory_totals_by_warehouse_tag
+                    ON inventory_totals_by_warehouse_tag.product_id = inventory.product_id
+                    AND inventory_totals_by_warehouse_tag.tag_id = taggables.tag_id
 
-        DB::insert("
-            INSERT INTO inventory_totals_by_warehouse_tag (
-                tag_id,
-                product_id,
-                quantity,
-                quantity_reserved,
-                quantity_available,
-                quantity_incoming,
-                max_inventory_updated_at,
-                created_at,
-                updated_at
-            )
-            SELECT
-                tempTable.tag_id as tag_id,
-                tempTable.product_id as product_id,
-                0 as quantity,
-                0 as quantity_reserved,
-                0 as quantity_available,
-                0 as quantity_incoming,
-                '2000-01-01 00:00:00' as max_inventory_updated_at,
-                NOW() as created_at,
-                NOW() as updated_at
+                WHERE
+                    taggables.taggable_type = 'App\\Models\\Warehouse'
+                    AND inventory_totals_by_warehouse_tag.id IS NULL
 
-            FROM tempTable
-        ");
+                LIMIT 5000;
+            ");
+
+            DB::insert("
+                INSERT INTO inventory_totals_by_warehouse_tag (
+                    tag_id,
+                    product_id,
+                    quantity,
+                    quantity_reserved,
+                    quantity_available,
+                    quantity_incoming,
+                    max_inventory_updated_at,
+                    created_at,
+                    updated_at
+                )
+                SELECT
+                    tempTable.tag_id as tag_id,
+                    tempTable.product_id as product_id,
+                    SUM(inventory.quantity) as quantity,
+                    SUM(inventory.quantity_reserved) as quantity_reserved,
+                    SUM(inventory.quantity_available) as quantity_available,
+                    SUM(inventory.quantity_incoming) as quantity_incoming,
+                    MAX(inventory.updated_at) as max_inventory_updated_at,
+                    NOW() as created_at,
+                    NOW() as updated_at
+
+                FROM tempTable
+                INNER JOIN inventory
+                    ON inventory.product_id = tempTable.product_id
+                    AND inventory.warehouse_id = tempTable.warehouse_id
+            ");
+            $maxRounds--;
+            Log::debug('EnsureTotalsByWarehouseTagRecordsExistJob: tempTable count ' . DB::table('tempTable')->count());
+        } while (DB::table('tempTable')->count() > 0 and $maxRounds > 0);
     }
 }
