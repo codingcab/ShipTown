41e6d9c45fc17cfdb28f09e36924f975d7577a59
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-15 15:19:39 +0200

    refactoring

--- app/Modules/InventoryTotals/src/Listeners/ProductCreatedEventListener.php
+++ app/Modules/InventoryTotals/src/Listeners/ProductCreatedEventListener.php
@@ -11,13 +11,29 @@ class ProductCreatedEventListener
 {
     public function handle(ProductCreatedEvent $event)
     {
+        $this->createInventoryTotalRecord($event);
+
+        $this->insertInventoryTotalsByWarehouseTagRecords($event);
+    }
+
+    /**
+     * @param ProductCreatedEvent $event
+     */
+    private function createInventoryTotalRecord(ProductCreatedEvent $event): void
+    {
         InventoryTotal::query()->create([
             'product_id' => $event->product->getKey(),
             'quantity' => 0,
             'quantity_reserved' => 0,
             'quantity_incoming' => 0,
         ]);
+    }
 
+    /**
+     * @param ProductCreatedEvent $event
+     */
+    private function insertInventoryTotalsByWarehouseTagRecords(ProductCreatedEvent $event): void
+    {
         $records = Taggable::query()
             ->where(['taggable_type' => 'App\Models\Warehouse'])
             ->get()
