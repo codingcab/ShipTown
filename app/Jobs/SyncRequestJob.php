c708cf146be7ba684d7c9e0d2be30dabb4499247
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-16 20:51:02 +0200

    make all jobs unique

--- app/Jobs/SyncRequestJob.php
+++ app/Jobs/SyncRequestJob.php
@@ -4,16 +4,11 @@ namespace App\Jobs;
 
 use App\Abstracts\UniqueJob;
 use App\Events\SyncRequestedEvent;
-use Illuminate\Support\Facades\Log;
 
 class SyncRequestJob extends UniqueJob
 {
     public function handle()
     {
-        Log::debug('SyncRequestedEvent dispatching');
-
         SyncRequestedEvent::dispatch();
-
-        Log::info('SyncRequestedEvent dispatched');
     }
 }
