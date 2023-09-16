ecde50d6cd1869176b17573c27cc108f8d84e0f5
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-17 00:57:45 +0200

    schedule job

--- app/Modules/InventoryTotals/src/Listeners/EveryTenMinutesEventListener.php
+++ app/Modules/InventoryTotals/src/Listeners/EveryTenMinutesEventListener.php
@@ -5,12 +5,14 @@ namespace App\Modules\InventoryTotals\src\Listeners;
 use App\Modules\InventoryTotals\src\Jobs\EnsureTotalsByWarehouseTagRecordsExistJob;
 use App\Modules\InventoryTotals\src\Jobs\EnsureTotalsRecordsExistJob;
 use App\Modules\InventoryTotals\src\Jobs\UpdateTotalsByWarehouseTagTableJob;
+use App\Modules\InventoryTotals\src\Jobs\UpdateTotalsTableJob;
 
 class EveryTenMinutesEventListener
 {
     public function handle()
     {
         EnsureTotalsRecordsExistJob::dispatch();
+        UpdateTotalsTableJob::dispatch();
 
         EnsureTotalsByWarehouseTagRecordsExistJob::dispatch();
         UpdateTotalsByWarehouseTagTableJob::dispatch();
