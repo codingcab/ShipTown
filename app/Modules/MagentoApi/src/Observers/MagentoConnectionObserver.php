69506b7ef9c8c18f53c74038c74554b43ca6b95c
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-22 16:30:46 +0200

    reset last_product_id_checked

--- app/Modules/MagentoApi/src/Observers/MagentoConnectionObserver.php
+++ app/Modules/MagentoApi/src/Observers/MagentoConnectionObserver.php
@@ -3,11 +3,14 @@
 namespace App\Modules\MagentoApi\src\Observers;
 
 use App\Modules\MagentoApi\src\Jobs\EnsureProductRecordsExistJob;
+use App\Modules\MagentoApi\src\Models\MagentoConfiguration;
 
 class MagentoConnectionObserver
 {
     public function created()
     {
+        MagentoConfiguration::query()->update(['last_product_id_checked' => 0]);
+
         EnsureProductRecordsExistJob::dispatch();
     }
 }
