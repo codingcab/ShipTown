3530d7da9c8b308543674bf82086a3d7a4b3fc25
arthur@coding.cab <arthur@youritsolutions.ie>
2023-12-09 18:39:19 +0100

    updates

--- resources/js/components/SharedComponents/BarcodeInputField.vue
+++ resources/js/components/SharedComponents/BarcodeInputField.vue
@@ -81,10 +81,16 @@
             console.log(window.navigator.userAgent);
             console.log(isIos());
             this.resetInputValue();
-            window.addEventListener('pageshow', (e) => {
-            this.setFocusOnBarcodeInput(200);
-                console.log('pageshow');
-            });
+            // window.addEventListener('pageshow', (e) => {
+            //     this.setFocusOnBarcodeInput(200);
+            //     console.log('pageshow');
+            // });
+
+
+            // Usage example
+            var myElement = document.getElementById('barcodeInput');
+            var modalFadeInDuration = 300;
+            this.focusAndOpenKeyboard(myElement, modalFadeInDuration);
 
             window.addEventListener('keydown', (e) => {
                 if (e.target.nodeName !== 'BODY') {
@@ -106,6 +112,33 @@
         },
 
         methods: {
+
+             focusAndOpenKeyboard(el, timeout) {
+        if(!timeout) {
+            timeout = 100;
+        }
+        if(el) {
+            // Align temp input element approximately where the input element is
+            // so the cursor doesn't jump around
+            var __tempEl__ = document.createElement('input');
+            __tempEl__.style.position = 'absolute';
+            __tempEl__.style.top = (el.offsetTop + 7) + 'px';
+            __tempEl__.style.left = el.offsetLeft + 'px';
+            __tempEl__.style.height = 0;
+            __tempEl__.style.opacity = 0;
+            // Put this temp element as a child of the page <body> and focus on it
+            document.body.appendChild(__tempEl__);
+            __tempEl__.focus();
+
+            // The keyboard is open. Now do a delayed focus on the target element
+            setTimeout(function() {
+                el.focus();
+                el.click();
+                // Remove the temp element
+                document.body.removeChild(__tempEl__);
+            }, timeout);
+        }
+    },
              simulateClick(control) {
                 if (document.all) {
                     control.click();
