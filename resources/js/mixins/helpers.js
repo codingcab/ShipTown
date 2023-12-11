c3f8dda976923665a532e7beca94c6e4d358dbee
arthur@coding.cab <arthur@youritsolutions.ie>
2023-12-11 20:29:36 +0100

    fix exit on double shelf label scan

--- resources/js/mixins/helpers.js
+++ resources/js/mixins/helpers.js
@@ -67,6 +67,7 @@ export default {
                 const isIos = () => !!window.navigator.userAgent.match(/AppleWebKit/i);
 
                 this.notifySuccess(window.navigator.userAgent);
+                this.notifySuccess(isIos( ));
 
                 if (isIos()) {
                     this.focusAndOpenKeyboard(element, delay);
