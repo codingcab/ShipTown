72a183d9e61e6a228acc34065a75fd767fda3bf4
Artur Hanusek <arthur@youritsolutions.ie>
2023-12-25 18:45:30 +0100

    refactoring

--- tests/Coverage/WebRoutesCoverageTest.php
+++ tests/Coverage/WebRoutesCoverageTest.php
@@ -46,7 +46,6 @@ class WebRoutesCoverageTest extends TestCase
     {
         $routeName = $route->uri . 'Test';
 
-
         $routeName = str_replace('-', '_', $routeName);
         $routeName = str_replace('.', '_', $routeName);
         $routeName = str_replace('{', '', $routeName);
