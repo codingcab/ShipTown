3f00bf158faf8ee443b9c3e04574012cefa6a063
Robert Goodliffe <robgoodliffe@gmail.com>
2024-02-29 18:04:40 +0100

    changes so far

--- app/Modules/InventoryReservations/src/Listeners/OrderProductCreatedEventListener.php
+++ app/Modules/InventoryReservations/src/Listeners/OrderProductCreatedEventListener.php
@@ -3,7 +3,11 @@
 namespace App\Modules\InventoryReservations\src\Listeners;
 
 use App\Events\OrderProduct\OrderProductCreatedEvent;
+use App\Models\Inventory;
 use App\Modules\InventoryReservations\src\Jobs\UpdateInventoryQuantityReservedJob;
+use App\Modules\InventoryReservations\src\Models\Configuration;
+use App\Modules\InventoryReservations\src\Models\InventoryReservation;
+use App\Modules\InventoryReservations\src\Services\ReservationsService;
 
 class OrderProductCreatedEventListener
 {
@@ -16,6 +20,26 @@ class OrderProductCreatedEventListener
      */
     public function handle(OrderProductCreatedEvent $event)
     {
+        $config = Configuration::first();
+
+        $inventory = Inventory::where('product_id', $event->orderProduct->product_id)
+            ->where('warehouse_id', $config->warehouse_id)
+            ->first(['id', 'warehouse_code', 'quantity_reserved']);
+
+        $inventoryReservationUuid = implode('_', ['order_product_id', $event->orderProduct->getKey()]);
+
+        InventoryReservation::create([
+            'inventory_id' => $inventory->id,
+            'product_sku' => $event->orderProduct->sku_ordered,
+            'warehouse_code' => $inventory->warehouse_code,
+            'quantity_reserved' => $event->orderProduct->quantity_to_ship,
+            'comment' => 'Order #' . $event->orderProduct->order->order_number, // Order #1
+            'custom_uuid' => $inventoryReservationUuid, // 123
+        ]);
+
+        ReservationsService::recalculateTotalQuantityReserved($inventory->id);
+
+        // todo - in what circumstances would product_id be null?
         if ($event->orderProduct->product_id === null) {
             return;
         }
--- app/Modules/InventoryReservations/src/Listeners/OrderProductUpdatedEventListener.php
+++ app/Modules/InventoryReservations/src/Listeners/OrderProductUpdatedEventListener.php
@@ -3,7 +3,10 @@
 namespace App\Modules\InventoryReservations\src\Listeners;
 
 use App\Events\OrderProduct\OrderProductUpdatedEvent;
+use App\Models\Inventory;
 use App\Modules\InventoryReservations\src\Jobs\UpdateInventoryQuantityReservedJob;
+use App\Modules\InventoryReservations\src\Models\InventoryReservation;
+use App\Modules\InventoryReservations\src\Services\ReservationsService;
 
 class OrderProductUpdatedEventListener
 {
@@ -15,10 +18,23 @@ class OrderProductUpdatedEventListener
      */
     public function handle(OrderProductUpdatedEvent $event)
     {
+        $inventoryReservationUuid = implode('_', ['order_product_id', $event->orderProduct->getKey()]);
+
+        $inventoryReservation = InventoryReservation::where('custom_uuid', $inventoryReservationUuid)->first();
+        $inventoryReservation->update([
+            'quantity_reserved' => $event->orderProduct->quantity_to_ship,
+        ]);
+
+        $inventory = Inventory::where('id', $inventoryReservation->inventory_id)->first();
+
+        ReservationsService::recalculateTotalQuantityReserved($inventory->id);
+
+        // todo - in what circumstances would product_id be null?
         if ($event->orderProduct->product_id === null) {
             return;
         }
 
         UpdateInventoryQuantityReservedJob::dispatchSync($event->orderProduct->product_id);
     }
+
 }
