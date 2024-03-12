f31ae7b796096a0da9e96cb3c5fe3492d8d7e8b0
Artur Hanusek <arturhanusek@Arturs-MacBook-Pro-2.local>
2024-03-12 12:17:56 +0100

    fix test

--- tests/Feature/Modules/ActiveOrdersInventoryReservations/BasicModuleTest.php
+++ tests/Feature/Modules/ActiveOrdersInventoryReservations/BasicModuleTest.php
@@ -26,6 +26,55 @@ class BasicModuleTest extends TestCase
     }
 
     /** @test */
+    public function testIfUpdatesInventoryReservationWhenOrderProductsIsModifiedOnActiveOrder()
+    {
+        // prepare database
+        $warehouse = Warehouse::factory()->create();
+
+        $product = Product::factory()->create();
+
+        $configuration = Configuration::firstOrCreate([]);
+        $configuration->update(['warehouse_id' => $warehouse->getKey()]);
+
+        $inventory = Inventory::where([
+            'product_id' => $product->getKey(),
+            'warehouse_id' => $warehouse->getKey(),
+        ])->first();
+
+        // doing something
+        $order = Order::factory()->create(['is_active' => true]);
+
+        $orderProduct1 = OrderProduct::factory()->create([
+            'order_id' => $order->getKey(),
+            'product_id' => $product->getKey(),
+        ]);
+
+        $orderProduct2 = OrderProduct::factory()->create([
+            'order_id' => $order->getKey(),
+            'product_id' => $product->getKey(),
+        ]);
+
+        $orderProduct1->update(['quantity_shipped' => $orderProduct1->quantity_ordered]);
+
+        // assert something
+        $this->assertDatabaseHas('inventory_reservations', [
+            'inventory_id' => $inventory->id,
+            'warehouse_code' => $warehouse->code,
+            'product_sku' => $orderProduct2->sku_ordered,
+            'quantity_reserved' => $orderProduct1->quantity_to_ship,
+            'comment' => 'Order #' . $order->order_number, // Order #1
+        ]);
+
+        $this->assertDatabaseHas('inventory_reservations', [
+            'inventory_id' => $inventory->id,
+            'warehouse_code' => $warehouse->code,
+            'product_sku' => $orderProduct2->sku_ordered,
+            'quantity_reserved' => $orderProduct2->quantity_to_ship,
+            'comment' => 'Order #' . $order->order_number, // Order #1
+        ]);
+    }
+
+    /** @test */
     public function testIfReservesInventoryWhenOrderProductsAddedToActiveOrder()
     {
         // prepare database
