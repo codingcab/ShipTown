7ecff2796cfaa7f54d700a34d7df345ca255c65a
Artur Hanusek <arturhanusek@Arturs-MacBook-Pro-2.local>
2024-03-19 13:23:05 +0100

    fix import

--- app/Modules/Rmsapi/src/Jobs/ProcessImportedProductRecordsJob.php
+++ app/Modules/Rmsapi/src/Jobs/ProcessImportedProductRecordsJob.php
@@ -142,7 +142,7 @@ class ProcessImportedProductRecordsJob extends UniqueJob
         ], [
             'inventory_id' => $importedProduct->inventory->id,
             'warehouse_code' => $importedProduct->inventory->warehouse_code,
-            'product_sku' => 'RMS Quantity Committed',
+            'product_sku' => $importedProduct->sku,
             'quantity_reserved' => data_get($importedProduct->raw_import, 'quantity_committed', 0),
             'comment' => 'Microsoft RMS - Imported Quantity Committed',
         ]);
