a3089099d6a72b1904f3958b944cc6e9a446f049
Robert Goodliffe <robgoodliffe@gmail.com>
2024-03-22 08:35:46 +0100

    migration

---
+++ database/migrations/2024_03_22_072347_add_auto_heal_job_class_column_to_heartbeats_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('heartbeats', function (Blueprint $table) {
+            // add auto_heal_job_class column
+            $table->string('auto_heal_job_class')->nullable->after('error_message');
+        });
+    }
+};
